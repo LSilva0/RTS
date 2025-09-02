da3f40115d4d952ac8cfaf9d3a943790fcaa26fa
Lucas Silva <lucas2070silva@gmail.com>
2025-09-02 11:19:08 +0000

    movement commit

--- unit.gd
+++ unit.gd
@@ -1,34 +1,44 @@
 extends CharacterBody2D
 
 @export var speed = 500
-@export var stop_distance = 10
-var target = null
+@export var target_radius = 10
+
 var stopped = true;
 var idle_tween = Tween;
+var av = Vector2.ZERO
+var avoid_weight = 0.2
+
+var selected = false:
+	set = set_selected
+	
+var target = null:
+	set = set_target
+	
 func _ready():
 	start_idle_animation()
 	
-func _input(event):
-	if event.is_action_pressed("set_target"):
-		target = get_global_mouse_position()
-		print('Input');
+
+#func _input(event):
+#	if event.is_action_pressed("set_target"):
+#		target = get_global_mouse_position()
+#		print('Input');
 
 func _physics_process(delta):
+	velocity = Vector2.ZERO
 	if target != null:
-		
-		var distance_to_target = position.distance_to(target)
-		
 		velocity = position.direction_to(target)
-		velocity = velocity.normalized() * speed
-		move_and_collide(velocity * delta)
-		if distance_to_target <= stop_distance:
+		rotation = lerp_angle(rotation,velocity.angle(), 0.5)
+		
+		if position.distance_to(target) <= target_radius:
 				target = null
 				velocity = Vector2.ZERO
-		else:
-			# Move towards target
-			var direction = position.direction_to(target)
-			velocity = direction * speed
-	if velocity != Vector2.ZERO:
+	
+		# Move towards target
+	av = avoid()
+	velocity = (velocity + av * avoid_weight).normalized() * speed
+	move_and_collide(velocity * delta)
+	print(velocity)
+	if velocity != Vector2.ZERO and stopped == true:
 		stopped = false
 		stop_idle_animation()
 	elif velocity == Vector2.ZERO and stopped == false:
@@ -36,7 +46,25 @@ func _physics_process(delta):
 		stopped = true
 		start_idle_animation()
 		
+func set_selected(value):
+	selected = value
+	if selected:
+		$Sprite2D.self_modulate = Color.AQUA
+	else:
+		$Sprite2D.self_modulate = Color.WHITE;
 
+func set_target(value):
+	target = value
+
+func avoid():
+	var result = Vector2.ZERO
+	var neighbors = $Detect.get_overlapping_bodies()
+	if neighbors:
+		for n in neighbors:
+			result += n.position.direction_to(position) #Averaging direction of neighbors to self - avoid vector
+		result /= neighbors.size() #average
+	return result.normalized();
+	
 func start_idle_animation():
 	
 	idle_tween = create_tween()
---
+++ world.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var dragging = false
+var drag_start = Vector2.ZERO
+var select_rect = RectangleShape2D.new()
+var selected = []
+
+@export var color_rect = ColorRect
+func _ready():
+	color_rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if selected.size() == 0: #Checks if there are units selected
+				dragging = true
+				drag_start = event.position
+			else:
+				for item in selected:
+					item.collider.target = event.position
+					item.collider.selected = false
+				selected = [] #Process every item, point them towards the event - mouse click - then unselect them
+		elif dragging:
+			dragging = false
+			queue_redraw()
+			var drag_end = event.position
+			select_rect.size = abs(drag_end - drag_start)
+			var space = get_world_2d().direct_space_state
+			var q = PhysicsShapeQueryParameters2D.new()
+			q.shape = select_rect
+			q.collision_mask = 2
+			q.transform = Transform2D(0, (drag_end + drag_start) / 2)
+			selected = space.intersect_shape(q) #Actual selection inside the rect area
+			for item in selected:
+				item.collider.selected = true
+	if event is InputEventMouseMotion and dragging:
+		queue_redraw()
+
+func _draw():
+	if dragging:
+		draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start), Color.AQUA, false, 2)
